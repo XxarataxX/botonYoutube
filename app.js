dec0abfa8cf36faac42feff2511c43fd787946d7
XxarataxX <74207904+XxarataxX@users.noreply.github.com>
2024-02-21 12:54:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
  
 // Middleware para servir archivos estáticos (si es necesario)
 app.use(express.static('public'));
